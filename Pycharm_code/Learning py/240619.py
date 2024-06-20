88ea570d2cbd6179c86319b20d64c566cd867153
Mellow-sky <1377639773@qq.com>
2024-06-20 21:41:59 +0800

    240620

---
+++ Pycharm_code/Learning py/240619.py
@@ -0,0 +1,34 @@
+# list = [21, 25, 21, 23, 22, 20]
+# list.append(31)
+# list2 = [29, 33, 30]
+# list.extend(list2)
+# print(list[0])
+# print(list[-1])
+# print(list.index(31))
+# print(list)
+
+
+# list = []
+# for i in range(1,11):
+#     list.append(i)
+# print(list)
+#
+# x = 0
+# list2 = []
+# while x < len(list):
+#     if(list[x] % 2 == 0):
+#         list2.append(list[x])
+#     x += 1
+#
+# print(list2)
+#
+# list3 = []
+# for i in list:
+#     if(i % 2 == 0):
+#         list3.append(i)
+# print(list3)
+
+
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+l = list[1:4]
+print(f"{l},{type(l)}")
