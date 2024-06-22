5f5aa086909443fe7db5cc8f17ce35bac0f29816
Mellow-sky <1377639773@qq.com>
2024-06-22 22:44:00 +0800

    240622

--- VSCode_code/cpp/240619/240619.cpp
+++ VSCode_code/cpp/240619/240619.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
+using ll = long long;
 int main(){
-    cout << "hello";
+    ll n, c;
+    cin >> n >> c;
+    vector<ll> a(n);
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    
+    
 
     return 0;
 }
