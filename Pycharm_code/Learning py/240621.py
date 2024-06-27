61fa543835566b9cafcc06bed3e2050492b608f4
Mellow-sky <1377639773@qq.com>
2024-06-27 14:48:13 +0800

    240627

--- Pycharm_code/Learning py/240621.py
+++ Pycharm_code/Learning py/240621.py
@@ -1,3 +1,19 @@
-list = [4, 7, 2, 9, 1, 5, 8, 3, 6, 4, 7, 2, 9, 1, 5, 8, 3, 6, 4, 7, 2, 9, 1, 5, 8, 3, 6]
-set = set(list)
-print(set)
+# list = [4, 7, 2, 9, 1, 5, 8, 3, 6, 4, 7, 2, 9, 1, 5, 8, 3, 6, 4, 7, 2, 9, 1, 5, 8, 3, 6]
+# set = set(list)
+# print(set)
+
+import tkinter as tk
+
+def on_button_click():
+    label.config(text="Hello, Tkinter!")
+
+root = tk.Tk()
+root.title("Simple GUI")
+
+label = tk.Label(root, text="Welcome!")
+label.pack()
+
+button = tk.Button(root, text="Click Me", command=on_button_click)
+button.pack()
+
+root.mainloop()
