9afed195e393e2a8750e144e24464bb04b899ad7
Mellow-sky <1377639773@qq.com>
2024-12-16 08:11:13 +0800

    241216

---
+++ VSCode_code/cpp/241215/241215.cpp
@@ -0,0 +1,157 @@
+// #include <iostream>
+// int main () {
+//     int t;
+//     std::cin >> t;
+//     while (t--) {
+//         int n;
+//         std::cin >> n;
+//         std::cout << n - 1 << "\n";
+//     } 
+//     return 0;
+// }
+
+
+
+// #include <iostream>
+// #include <string>
+// int main () {
+//     int t;
+//     std::cin >> t;
+//     while (t--) {
+//         std::string s;
+//         std::cin >> s;
+//         for (int i = s.size() - 1; i >= 0; i--) {
+//             if (s[i] == 'p') {
+//                 std::cout << 'q';
+//             } else if (s[i] == 'q') {
+//                 std::cout << 'p';
+//             } else {
+//                 std::cout << s[i];
+//             }
+//         }
+//         std::cout << "\n";
+//     }
+//     return 0;
+// }
+
+
+// #include <iostream>
+// void vol() {
+//     int m, a, b, c;
+//     std::cin >> m >> a >> b >> c;
+//     int ans = 0;
+//     if (a < m) {
+//         ans += a;
+//         if (c > 0 && c < m - a) {
+//             ans += c;
+//             c = 0;
+//         } else if (c > 0 && c >= m - a) {
+//             ans += m - a;
+//             c -= m - a;
+//         }
+//     } else {
+//         ans += m;
+//     }
+//     if (b < m) {
+//         ans += b;
+//         if (c > 0 && c < m - b) {
+//             ans += c;
+//             c = 0;
+//         } else if (c > 0 && c >= m - b) {
+//             ans += m - b;
+//             c -= m - b;
+//         }
+//     } else {
+//         ans += m;
+//     }
+//     std::cout << ans << "\n";
+// }
+// int main () {
+//     int  t;
+//     std::cin >> t;
+//     while (t--) {
+//         vol();
+//     }
+//     return 0;
+// }
+
+
+
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// bool b = true;
+// int n;
+// std::vector<int> a(n), b(n, 1), c(n, 1);
+// void updata(std::vector<int> &b) {
+//     bool tep = true;
+//     for (auto &i : b) {
+//         if (i > 1) {
+//             i = 0;
+//             tep = false;
+//         }
+//     }
+//     if (tep) {
+//         b = false;
+//     }
+// }
+// void git(auto &a, auto &b) {
+//     for (int i = 0; i < n; i++) {
+//         if (b[i] == 1) {
+//             c[a[i]]++;
+//             b[i]--;
+//         }
+//     }
+// }
+// void vol() {
+//     std::cin >> n;
+//     for (int i = 0; i < n;i++) {
+//         std::cin >> a[i];
+//     }
+//     int ans = 0;
+//     b = true;
+//     while (b) {
+//         git(a, b);
+//         updata(b);
+//         ans++;
+//     }
+// }
+// int main () {
+//     int t;
+//     std::cin >> t;
+//     while (t--) {
+//         vol();
+//     }
+//     return 0;
+// }
+
+
+
+// #include <iostream>
+// #include <vector>
+// void vol() {
+//     int n;
+//     std::cin >> n;
+//     std::vector<int> a(n), b(n + 1);
+//     for (int i = 0; i < n; i++) {
+//         std::cin >> a[i];
+//         if (b[a[i]] == 0) {
+//             std::cout << a[i] << " ";
+//             b[a[i]]++;
+//         }
+//     }
+//     for (int i = 1; i <= n; i++) {
+//         if (b[i] == 0) {
+//             std::cout << i << " ";
+//         }
+//     }
+//     std::cout << "\n";
+// }
+// int main () {
+//     int t;
+//     std::cin >> t;
+//     while (t--) {
+//         vol();
+//     }
+//     return 0;
+// }
